ae8914a182f4a9d051f72e0296242683638c2376
CJRCODER <cjrcoder@gmail.com>
2026-05-29 11:03:19 +0530

    test(math): verify mathHelper3 logic

--- src/utils/math.test.js
+++ src/utils/math.test.js
@@ -2,8 +2,10 @@ import { test } from 'node:test';
 import assert from 'node:assert';
 import {
   mathHelper1,
-  mathHelper2
+  mathHelper2,
+  mathHelper3
 } from './math.js';
 
 test('mathHelper1', () => { assert.strictEqual(mathHelper1(1), 2); });
 test('mathHelper2', () => { assert.strictEqual(mathHelper2(1), 3); });
+test('mathHelper3', () => { assert.strictEqual(mathHelper3(1), 4); });
