411b1c4f86273edc2afadce244873ee14e4c0932
CJRCODER <cjrcoder@gmail.com>
2026-05-29 11:03:26 +0530

    test(math): verify mathHelper6 logic

--- src/utils/math.test.js
+++ src/utils/math.test.js
@@ -5,7 +5,8 @@ import {
   mathHelper2,
   mathHelper3,
   mathHelper4,
-  mathHelper5
+  mathHelper5,
+  mathHelper6
 } from './math.js';
 
 test('mathHelper1', () => { assert.strictEqual(mathHelper1(1), 2); });
@@ -13,3 +14,4 @@ test('mathHelper2', () => { assert.strictEqual(mathHelper2(1), 3); });
 test('mathHelper3', () => { assert.strictEqual(mathHelper3(1), 4); });
 test('mathHelper4', () => { assert.strictEqual(mathHelper4(1), 5); });
 test('mathHelper5', () => { assert.strictEqual(mathHelper5(1), 6); });
+test('mathHelper6', () => { assert.strictEqual(mathHelper6(1), 7); });
